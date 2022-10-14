193ef03b5c80fb795a09d94492c4b1649eb5aee0
Jer Warren <jerwarren@gmail.com>
2022-10-14 12:30:34 -0700

    semicolon

--- src/android/NotificationCommands.java
+++ src/android/NotificationCommands.java
@@ -19,7 +19,7 @@ import android.os.Bundle;
 import android.graphics.Bitmap;
 import java.io.ByteArrayOutputStream;
 import android.util.Base64;
-import android.app.Notification
+import android.app.Notification;
 
 public class NotificationCommands extends CordovaPlugin {
 
